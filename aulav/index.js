6064974533202be65c77824b40691642dc5d0bcc
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-10 11:53:02 -0300

    topString

--- aulav/index.js
+++ aulav/index.js
@@ -3,3 +3,8 @@ console.log("HELLOW WOLRD")
 const nome = "João"
 const idade = 17
 console.log("Meu nome é",nome,"e tenho",idade,"de idade")
+//Teste
+const idadeNumero = 17
+const idadeNumero = idadeNumero.topString()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
