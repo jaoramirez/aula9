609d8d7cbcd0d2b90a61b84c06bfbc67fb38d9fb
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-11 11:59:57 -0300

    a=25 e b=10

--- template_javascript/index.js
+++ template_javascript/index.js
@@ -5,6 +5,16 @@
 //Ele é indefinido pois o let n tem que ter uma definição
 //console.log("Qual é o seu?",nome,"e quantos anos vc tem?",idade,)
 
-let nome = prompt("Digite sue nome:")
-let idade = prompt("digite sua idade:")
-alert(`Seu nome é ${nome} e você tem ${idade} anos de idade`)
+//let nome = prompt("Digite sue nome:")
+//let idade = prompt("digite sua idade:")
+//alert(`Seu nome é ${nome} e você tem ${idade} anos de idade`)
+
+let a = 10
+let b = 25
+
+ a = 25
+ b = 10
+
+// Depois de trocados, teremos o seguinte resultado:
+console.log("O novo valor de a é", a) // O novo valor de a é 25
+console.log("O novo valor de b é", b) // O novo valor de b é 10
