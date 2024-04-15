5ffe2c3eac1054f476560a2a4cb0012d9cfe5991
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-15 09:52:02 -0300

    trab front

--- template_javascript/index.js
+++ template_javascript/index.js
@@ -1,20 +1,24 @@
-
-//let nome
-//let idade 
-//console.log(typeof nome)
-//Ele é indefinido pois o let n tem que ter uma definição
-//console.log("Qual é o seu?",nome,"e quantos anos vc tem?",idade,)
-
-//let nome = prompt("Digite sue nome:")
-//let idade = prompt("digite sua idade:")
-//alert(`Seu nome é ${nome} e você tem ${idade} anos de idade`)
-
-let a = 10
-let b = 25
-
- a = 25
- b = 10
-
-// Depois de trocados, teremos o seguinte resultado:
-console.log("O novo valor de a é", a) // O novo valor de a é 25
-console.log("O novo valor de b é", b) // O novo valor de b é 10
+// Receba as variáveis 
+let nome = prompt("Digite seu nome:")
+let idade = prompt("Digite sua idade:") 
+//MENSAGEN: Digite as horas que vc trabalha por dia 
+let horasTrabalho = prompt("Digite as horas em que você trabalha por dia:")
+let salarioDia = prompt("Digite o seu salário diário:")
+// Verificando as variáveis 
+console.log(typeof nome)
+console.log(typeof horasTrabalho)
+console.log(typeof salarioDia)
+// converte o que é nessesário 
+let horasTrabNumber = Number(horasTrabalho)
+let saldiasNumber = Number(salarioDia)
+// processando valores
+let valHoraNumber = horasTrabNumber/saldiasNumber
+// Convesão para não dar erro no NULL
+let valorHora = String(valHoraNumber)
+const mensagem = "Seu valor hora e "+ valorHora + "!!!"
+// Verificando variáveis
+console.log(typeof valorHora)
+console.log(typeof mensagem)
+console.log(mensagem)
+//Saída de valores pra Front End
+alert(mensagem)
