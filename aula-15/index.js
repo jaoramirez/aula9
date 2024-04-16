7a560d09606b6b2e4a01ec712661e555b78ee283
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 10:12:19 -0300

    === !== < >

---
+++ aula-15/index.js
@@ -0,0 +1,37 @@
+//let resultado
+
+//resultado = 3 + 4
+///alert(resultado)
+
+//resultado = (3+5)/2
+//alert(resultado)
+
+//resultado = 3+5/2
+//alert(resultado)
+
+//resultado = (4-5)*-1
+//alert(resultado)
+
+//resultado = 234 % 5 
+//alert(resultado)
+
+let primeiroNumero 
+let segundoNumero
+let resultado 
+
+//Leitura de dados informados pelpo úsuario
+primeiroNumero = prompt("Digite o primeiro numero") //3
+segundoNumero = prompt("Digite o segunfo numero")   //3
+
+//teste de comparação
+resultado = primeiroNumero === segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero !== segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero < segundoNumero
+alert(resultado)
+
+resultado = primeiroNumero > segundoNumero
+alert(resultado)
