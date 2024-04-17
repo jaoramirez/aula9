909e588736cff58a521ad38c4759bc5e3b7cbd0c
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-17 11:25:07 -0300

    4 atividade

--- aula-17/index.js
+++ aula-17/index.js
@@ -17,11 +17,28 @@ alert(`a difereça de idade é ${diferençadeidade}`)*/
 
 //console.log((numeroUm % 2)== 0)
 
-let idadeAnos 
+/*let idadeAnos 
 idadeAnos = Number(prompt("quantos anos você tem ?"))
 //idade em meses
 alert(idadeAnos * 12)
 // idade em dias 
 alert(idadeAnos * 365)
 //idade em horas 
-alert(idadeAnos * 8766)
+alert(idadeAnos * 8766)*/
+
+let num1
+let num2
+num1 = Number(prompt("Escolha um númer:"))
+num2 = Number(prompt("Escolha outro númer:"))
+
+resultado = num1 >= num2 
+alert(`O primeiro numero é maior que segundo? ${resultado}`)
+
+resultado2 = num1 === num2
+alert(`O primeiro numero é igual ao segundo? ${resultado2}`)
+ 
+resultado3 = (num1 % num2) == 0 
+alert(`O primeiro numero é divisível pelo segundo? ${resultado3}`) 
+
+resultado4 = (num2 % num1) == 0
+alert(`O segundo numero é divisível pelo primeiro? ${resultado4}`)
