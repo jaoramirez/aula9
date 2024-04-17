a05786a47bca433ea452ea4d70ecbb473d4c7d35
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-17 10:33:05 -0300

    idade em diferentes tempos

--- aula-17/index.js
+++ aula-17/index.js
@@ -11,8 +11,17 @@ alert(`sua idade é mairo que a do seu amigo? ${resultado}`)
 diferençadeidade = idade - idadeAmigo
 alert(`a difereça de idade é ${diferençadeidade}`)*/
 
-let numeroUm 
+//let numeroUm 
 
-numeroUm = prompt("Digite um número par:")
+//numeroUm = prompt("Digite um número par:")
 
-console.log((numeroUm % 2)== 0)
+//console.log((numeroUm % 2)== 0)
+
+let idadeAnos 
+idadeAnos = Number(prompt("quantos anos você tem ?"))
+//idade em meses
+alert(idadeAnos * 12)
+// idade em dias 
+alert(idadeAnos * 365)
+//idade em horas 
+alert(idadeAnos * 8766)
