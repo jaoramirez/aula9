f7532a37eeeeeb394926c72fba9796bec352ec0a
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-17 11:49:14 -0300

    desafio 1

--- aula-17/index.js
+++ aula-17/index.js
@@ -26,7 +26,7 @@ alert(idadeAnos * 365)
 //idade em horas 
 alert(idadeAnos * 8766)*/
 
-let num1
+/*let num1
 let num2
 num1 = Number(prompt("Escolha um númer:"))
 num2 = Number(prompt("Escolha outro númer:"))
@@ -41,4 +41,113 @@ resultado3 = (num1 % num2) == 0
 alert(`O primeiro numero é divisível pelo segundo? ${resultado3}`) 
 
 resultado4 = (num2 % num1) == 0
-alert(`O segundo numero é divisível pelo primeiro? ${resultado4}`)
+alert(`O segundo numero é divisível pelo primeiro? ${resultado4}`)*/
+
+/*let idade 
+let idadeAmigo
+let diferençadeidade
+
+idade = prompt("Qual é a sua idade?")
+idadeAmigo = prompt("Qual é a idade do seu amigo")
+
+resultado = idade >= idadeAmigo
+alert(`sua idade é mairo que a do seu amigo? ${resultado}`)
+
+diferençadeidade = idade - idadeAmigo
+alert(`a difereça de idade é ${diferençadeidade}`)*/
+
+//let numeroUm 
+
+//numeroUm = prompt("Digite um número par:")
+
+//console.log((numeroUm % 2)== 0)
+
+/*let idadeAnos 
+idadeAnos = Number(prompt("quantos anos você tem ?"))
+//idade em meses
+alert(idadeAnos * 12)
+// idade em dias 
+alert(idadeAnos * 365)
+//idade em horas 
+alert(idadeAnos * 8766)*/
+
+/*let num1
+let num2
+num1 = Number(prompt("Escolha um númer:"))
+num2 = Number(prompt("Escolha outro númer:"))
+
+resultado = num1 >= num2 
+alert(`O primeiro numero é maior que segundo? ${resultado}`)
+
+resultado2 = num1 === num2
+alert(`O primeiro numero é igual ao segundo? ${resultado2}`)
+ 
+resultado3 = (num1 % num2) == 0 
+alert(`O primeiro numero é divisível pelo segundo? ${resultado3}`) 
+
+resultado4 = (num2 % num1) == 0
+alert(`O segundo numero é divisível pelo primeiro? ${resultado4}`)*/
+
+/*let idade 
+let idadeAmigo
+let diferençadeidade
+
+idade = prompt("Qual é a sua idade?")
+idadeAmigo = prompt("Qual é a idade do seu amigo")
+
+resultado = idade >= idadeAmigo
+alert(`sua idade é mairo que a do seu amigo? ${resultado}`)
+
+diferençadeidade = idade - idadeAmigo
+alert(`a difereça de idade é ${diferençadeidade}`)*/
+
+//let numeroUm 
+
+//numeroUm = prompt("Digite um número par:")
+
+//console.log((numeroUm % 2)== 0)
+
+/*let idadeAnos 
+idadeAnos = Number(prompt("quantos anos você tem ?"))
+//idade em meses
+alert(idadeAnos * 12)
+// idade em dias 
+alert(idadeAnos * 365)
+//idade em horas 
+alert(idadeAnos * 8766)*/
+
+/*let num1
+let num2
+num1 = Number(prompt("Escolha um númer:"))
+num2 = Number(prompt("Escolha outro númer:"))
+
+resultado = num1 >= num2 
+alert(`O primeiro numero é maior que segundo? ${resultado}`)
+
+resultado2 = num1 === num2
+alert(`O primeiro numero é igual ao segundo? ${resultado2}`)
+ 
+resultado3 = (num1 % num2) == 0 
+alert(`O primeiro numero é divisível pelo segundo? ${resultado3}`) 
+
+resultado4 = (num2 % num1) == 0
+alert(`O segundo numero é divisível pelo primeiro? ${resultado4}`)*/
+
+alert("a) Calcule e mostre o valor de 77°F em  K, mostrando a unidade no console também.")
+a = (77 - 32)*(5/9) + 273.15
+alert(a)
+
+alert("b) Calcule e mostre o valor de 80°C em °F, mostrando a unidade no console também.")
+b =  (80)*(9/5) + 32
+alert(b)
+
+alert("c) Calcule e mostre o valor de 30°C em °F e K, mostrando as unidades no console também")
+let umc = (30)*(9/5)+ 32  
+let doisc = (30 - 32)*(5/9) + 273.15
+alert(`${umc} e ${doisc}`)
+
+alert("d) Altere o último item para que o usuário insira o valor em graus Celsius que ele deseja converter")
+d = prompt("escolha um número")
+let umd = (d)*(9/5)+ 32  
+let doisd = (d - 32)*(5/9) + 273.15
+alert(`${umd} e ${doisd}`)
