a469a3df5e93f19d48276a7f195a4ac93bec82bb
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-17 09:41:15 -0300

    git1

---
+++ aula-17/index.js
@@ -0,0 +1,12 @@
+let idade 
+let idadeAmigo
+let diferençadeidade
+
+idade = prompt("Qual é a sua idade?")
+idadeAmigo = prompt("Qual é a idade do seu amigo")
+
+resultado = idade >= idadeAmigo
+alert(`sua idade é mairo que a do seu amigo? ${resultado}`)
+
+diferençadeidade = idade - idadeAmigo
+alert(`a difereça de idade é ${diferençadeidade}`)
