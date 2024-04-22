e8eeb12ae65398157b3f03129ff6cc0ba8cf4286
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-22 11:29:59 -0300

    exercícios

--- aula-22/index.js
+++ aula-22/index.js
@@ -26,11 +26,31 @@ console.log('f. ', valor)
 let email = prompt("Qual é o seu email?")
 alert(`O e-mail ${email} foi cadastrado com sucesso. Seja bem-vinda(o), ${nome} !`)*/
 
-const listaDeComidas = ["Pizaa", "Lasanha", "Hamburguer", "Bolo", "Massa"]
+/*const listaDeComidas = ["Pizaa", "Lasanha", "Hamburguer", "Bolo", "Massa"]
 console.log(listaDeComidas) 
 console.log("Essas são as minhas comidas preferidas:" + "\n" + listaDeComidas[0] + "\n" + listaDeComidas[1] + "\n" + listaDeComidas[2] + "\n" + listaDeComidas[3] + "\n" + listaDeComidas[4])
 alert("Essas são as minhas comidas preferidas:" + "\n" + listaDeComidas[0] + "\n" + listaDeComidas[1] + "\n" + listaDeComidas[2] + "\n" + listaDeComidas[3] + "\n" + listaDeComidas[4])
 
 
 const comida1 = prompt("E qual é a sua comida favorita?")
-alert("Essas são as minhas comidas preferidas:" + "\n" + comida1 + "\n" + listaDeComidas[1] + "\n" + listaDeComidas[2] + "\n" + listaDeComidas[3] + "\n" + listaDeComidas[4])
+alert("Essas são as minhas comidas preferidas:" + "\n" + comida1 + "\n" + listaDeComidas[1] + "\n" + listaDeComidas[2] + "\n" + listaDeComidas[3] + "\n" + listaDeComidas[4])*/
+
+let array
+array = []
+let listaDeTarefas = array
+
+let tarefasUsuario = prompt("Digite uma tarefa que você precise realizar no dia de hoje:")
+    listaDeTarefas.push(tarefasUsuario)
+    console.log(listaDeTarefas)
+    
+    tarefasUsuario = prompt("Digite uma tarefa que você precise realizar no dia de hoje: ")
+    listaDeTarefas.push(tarefasUsuario)
+    console.log(listaDeTarefas)
+    
+    tarefasUsuario = prompt("Digite uma tarefa que você precise realizar no dia de hoje: ")
+    listaDeTarefas.push(tarefasUsuario)
+    console.log(listaDeTarefas)
+
+let indiceUsuario = prompt("Digite o índice de uma tarefa que já realizou: ")
+listaDeTarefas.splice(indiceUsuario,1)
+console.log(listaDeTarefas)
