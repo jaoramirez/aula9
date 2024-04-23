8a8aeb9a3cba60d9e9491c0cb3cbb7af42db0163
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-23 10:16:19 -0300

    atividade3

--- aula-23/index.js
+++ aula-23/index.js
@@ -1,8 +1,21 @@
-let num1 = prompt("Escolha um numero:")
-let num2 = prompt("Escolha outro numero:")
+//Exercício 1,2 e 3
+/*let num1 = prompt("Escolha um número:")
+let num2 = prompt("Escolha outro número:")
 
 if (num1 === num2){
-alert("Parabens o numeros são iguais")
+alert("Parabens, os números são iguais!!!")
+} else { 
+alert("Eles não são iguais!")
+}
+*/
+
+let num1 = prompt("Escolha um número:")
+let num2 = prompt("Escolha outro número:")
+
+if (num1 === num2){
+alert("Parabens, os números são iguais!!!")
+} else if (num1 <= num2){
+alert ("O segundo é maior que o primeiro!!")
 } else {
-alert("Eles não são iguais")
+alert("O primeiro é maior que o segundo!!")
 }
