ce8672b11188c2496d47de677c1a7c043115b20a
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-23 09:56:49 -0300

    atividade 1

---
+++ aula-23/index.js
@@ -0,0 +1,8 @@
+let num1 = prompt("Escolha um numero:")
+let num2 = prompt("Escolha outro numero:")
+
+if (num1 === num2){
+alert("Parabens o numeros são iguais")
+} else {
+alert("Eles não são iguais")
+}
