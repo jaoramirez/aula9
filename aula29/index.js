a87f41ce29ed11fb0ab3c726c098227829ca72ab
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-29 11:34:03 -0300

    trabalho de comclução

---
+++ aula29/index.js
@@ -0,0 +1,26 @@
+// Receba as variáveis 
+let nome = prompt("Digite seu nome:")
+let idade = prompt("Digite sua idade:") 
+//MENSAGEN: Digite as horas que vc trabalha por dia 
+let horasTrabalho = prompt("Digite as horas em que você trabalha por dia:")
+let salarioDia = prompt("Digite o seu salário diário:")
+// Verificando as variáveis 
+console.log(typeof nome)
+console.log(typeof horasTrabalho)
+console.log(typeof salarioDia)
+// converte o que é nessesário 
+let horasTrabNumber = Number(horasTrabalho)
+let salDiaNumber = Number(salarioDia)
+// processando valores
+let valHoraNumber = horasTrabNumber/salDiaNumber
+// Convesão para não dar erro no NULL
+let valorHora = String(valHoraNumber)
+const mensagem = "Seu valor hora e "+ valorHora + "!!!"
+// Verificando variáveis
+console.log(typeof valorHora)
+console.log(typeof mensagem)
+console.log(mensagem)
+//Saída de valores pra Front End
+alert(mensagem)
+
+const 
